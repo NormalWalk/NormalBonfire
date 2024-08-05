28822e2f95efdf3eeaac8d755409ea54f24bd22a
NormalWalk_ <144937132+NormalWalk@users.noreply.github.com>
2024-08-05 17:05:48 +0700

    Color.java

--- src/main/java/ru/normalwalk/normalbonfire/Color.java
+++ src/main/java/ru/normalwalk/normalbonfire/Color.java
@@ -1,2 +1,30 @@
-package ru.normalwalk.normalbonfire;public class Color {
+package ru.normalwalk.normalbonfire;
+
+import net.md_5.bungee.api.ChatColor;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Color {
+    public static String color(String text) {
+        Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+
+        for(Matcher matcher = pattern.matcher(text); matcher.find(); matcher = pattern.matcher(text)) {
+            String hexCode = text.substring(matcher.start(), matcher.end());
+            String replaceSharp = hexCode.replace('#', 'x');
+            StringBuilder builder = new StringBuilder();
+            replaceSharp.chars().forEach((c) -> {
+                builder.append("&").append((char)c);
+            });
+            text = text.replace(hexCode, builder.toString());
+        }
+
+        return ChatColor.translateAlternateColorCodes('&', text).replace("&", "");
+    }
+
+    public static List<String> color(List<String> text) {
+        return (List)text.stream().map(Color::color).collect(Collectors.toList());
+    }
 }
+
